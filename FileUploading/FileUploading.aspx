feedcf71059f6c41bb6cca4c26076685a1d84bfe
CodeWithGopi <79987677+gpaptechnologies@users.noreply.github.com>
2022-04-20 23:13:56 +0530

    Add files via upload

---
+++ FileUploading/FileUploading.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FileUploading.aspx.cs" Inherits="FileUploading.FileUploading" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>File uploading in asp.net</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Table ID="tblFileUpload" runat="server" HorizontalAlign="Center" BorderColor="Black" BorderStyle="Solid" BorderWidth="1">
+                <asp:TableHeaderRow>
+                    <asp:TableHeaderCell Font-Bold="true" Font-Size="X-Large" ColumnSpan="3" ForeColor="Blue">
+                FILE UPLOADING<hr />
+                    </asp:TableHeaderCell>
+                </asp:TableHeaderRow>
+                <asp:TableRow>
+                    <asp:TableCell Font-Bold="true" VerticalAlign="Middle">
+                        <asp:Label ID="lblSelectFile" runat="server" Text="Select File"></asp:Label>
+                    </asp:TableCell>
+                    <asp:TableCell VerticalAlign="Middle">
+                        <asp:FileUpload ID="fuFileUpload" runat="server" ForeColor="YellowGreen" Font-Bold="true" />
+                    </asp:TableCell>
+                    <asp:TableCell VerticalAlign="Middle">
+                        <asp:Button ID="btnUpload" runat="server" Text="Upload File" OnClick="btnUpload_Click" BackColor="YellowGreen" Font-Bold="true" Height="30px" />
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableFooterRow>
+                    <asp:TableCell ColumnSpan="3">
+                        <hr />
+                        <asp:Label ID="lblMessage" runat="server" ForeColor="Green" Font-Bold="true"></asp:Label>
+                    </asp:TableCell>
+                </asp:TableFooterRow>
+            </asp:Table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ FileUploading/FileUploading.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FileUploading
+{
+    public partial class FileUploading : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                lblMessage.Text = string.Empty;
+            }
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            UploadFile();
+        }
+
+        void UploadFile()
+        {
+            try
+            {
+                if (fuFileUpload.HasFile)
+                {
+                    //Check File Extension
+                    string fileExtension = System.IO.Path.GetExtension(fuFileUpload.FileName);
+
+                    if (fileExtension.ToLower() != ".doc" && fileExtension.ToLower() != ".docx" && fileExtension.ToLower() != ".xls" && fileExtension.ToLower() != ".xlsx")
+                    {
+                        lblMessage.Text = "Files with .doc, .docsx, .xls and .xlsx only allowed.";
+                        lblMessage.ForeColor = System.Drawing.Color.Red;
+                    }
+                    else
+                    {
+                        //Check file size
+                        int fileSize = fuFileUpload.PostedFile.ContentLength / 1024;
+
+                        //Allow less than 3 MB only(1024*3)
+                        if (fileSize > 3072)
+                        {
+                            lblMessage.Text = "File size should be less than 3 MB only.";
+                        }
+                        else
+                        {
+                            string folderPath = Server.MapPath("~/UploadedFiles/");
+
+                            //Check whether Directory is available or not.
+                            if (!Directory.Exists(folderPath))
+                            {
+                                //if directory is not available, then create a folder.
+                                Directory.CreateDirectory(folderPath);
+                            }
+
+                            //save the file to folder
+                            fuFileUpload.SaveAs(folderPath + Path.GetFileName(fuFileUpload.FileName));
+
+                            //dispaly message after uploaded
+                            lblMessage.Text = "File " + Path.GetFileName(fuFileUpload.FileName) + " uploaded successfully...!";
+                        }
+                    }
+                }
+                else
+                {
+                    lblMessage.Text = "Please select a file to upload.";
+                    lblMessage.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = ex.Message;
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+    }
+}
